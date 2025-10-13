59bfca7226cdc79beb6a6166e9aacdd6f9878b88
Agam Harpreet Singh <136053043+Agam77055@users.noreply.github.com>
2025-10-13 17:07:13 +0530

    Update TeamSection.tsx

--- components/sections/TeamSection.tsx
+++ components/sections/TeamSection.tsx
@@ -11,8 +11,8 @@ const teamMembers = [
     image: 'https://media.licdn.com/dms/image/v2/D4D03AQG9grbISONcVg/profile-displayphoto-shrink_800_800/B4DZb63p7PGkAc-/0/1747965618989?e=1762992000&v=beta&t=ie7NcucVmgohrnc1rTsyajxg7ujWXK3ywJSTBCbOR0c',
     bio: 'Serving as General Secretary ACAC',
     social: {
-      linkedin: '#',
-      email: 'yashgolani@example.com',
+      linkedin: 'https://www.linkedin.com/in/yash-golani-144210106/',
+      email: 'gensecy_acac@iitj.ac.in',
       github: '#'
     }
   },
@@ -24,8 +24,8 @@ const teamMembers = [
     image: 'https://media.licdn.com/dms/image/v2/D5603AQF3GJ8gI-p5Xg/profile-displayphoto-shrink_400_400/B56Zaa7cMFG4Ag-/0/1746355996152?e=1762992000&v=beta&t=Tgr7oapl5sfPpKuTDnA6IZ_U8usMyt3pfLCZDpytwnY',
     bio: 'Serving as General Secretary SS',
     social: {
-      linkedin: '#',
-      email: 'tharakadatta@example.com',
+      linkedin: 'https://www.linkedin.com/in/tharakadattahegde/',
+      email: 'gensecy_ss@iitj.ac.in',
       github: '#'
     }
   },
@@ -37,8 +37,8 @@ const teamMembers = [
     image: 'https://media.licdn.com/dms/image/v2/D5603AQHySTqEmnzraQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1692857602743?e=1762992000&v=beta&t=rM6cvduJjZSBOJNiMDsOAMie2WHtA_nb4A1uIC0_9h0',
     bio: 'Vice President of BCCA',
     social: {
-      linkedin: '#',
-      email: 'sambhavjha@example.com',
+      linkedin: 'https://www.linkedin.com/in/sambhav-jha-61412528a/',
+      email: 'b23ee1092@iitj.ac.in',
       github: '#'
     }
   },
@@ -47,11 +47,11 @@ const teamMembers = [
     name: 'Dhruva Kumar Kaushal',
     role: 'Coordinating Team',
     department: 'Student Team',
-    image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
+    image: 'https://media.licdn.com/dms/image/v2/D5603AQGfYHSN5GlbtQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1726773078669?e=1762992000&v=beta&t=nIngUzfwmFqZ1b71red2SW1_kQG4X-D6uo8kC1IzfzY',
     bio: 'Member of the Coordinating Team',
     social: {
-      linkedin: '#',
-      email: 'dhruva@example.com',
+      linkedin: 'https://www.linkedin.com/in/dhruvakkaushal/',
+      email: 'b22ai017@iitj.ac.in',
       github: '#'
     }
   },
@@ -60,11 +60,11 @@ const teamMembers = [
     name: 'Rhythm Baghel',
     role: 'Coordinating Team',
     department: 'Student Team',
-    image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
+    image: 'https://media.licdn.com/dms/image/v2/D5603AQGJQUzlv-QuPg/profile-displayphoto-scale_400_400/B56Zjtn9OZHUAg-/0/1756333333701?e=1762992000&v=beta&t=hpz4L5GUEk3zPSkANx4AQzetGxlmhvTBJDJ-cPpmNwI',
     bio: 'Member of the Coordinating Team',
     social: {
-      linkedin: '#',
-      email: 'rhythm@example.com',
+      linkedin: 'https://www.linkedin.com/in/rhythm-baghel-80675a25a/',
+      email: 'b22cs042@iitj.ac.in',
       github: '#'
     }
   },
@@ -73,11 +73,11 @@ const teamMembers = [
     name: 'Anuj Patil',
     role: 'Coordinating Team',
     department: 'Student Team',
-    image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
+    image: 'https://media.licdn.com/dms/image/v2/D4D35AQGagL9VHkBnnQ/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1721658723725?e=1760961600&v=beta&t=7MW2XjJ3ISKiBhr2F6cgaOH7v7vj3EUZhXsOdboR5cY',
     bio: 'Member of the Coordinating Team',
     social: {
-      linkedin: '#',
-      email: 'anuj@example.com',
+      linkedin: 'https://www.linkedin.com/in/anuj-vijay-patil/',
+      email: 'b22ee010@iitj.ac.in',
       github: '#'
     }
   },
@@ -86,11 +86,11 @@ const teamMembers = [
      name: 'Mrudhul Tula',
     role: 'Core Team',
     department: 'Student Team',
-    image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
+    image: 'https://media.licdn.com/dms/image/v2/D4E35AQFT13ALYPjeRw/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1737932009273?e=1760961600&v=beta&t=ZYnMYD5eScuBXa1CKiSCFalkQ8G_xAFmCP79hetpv2w',
     bio: 'Member of the Core Team',
     social: {
-      linkedin: '#',
-      email: 'mrudhul@example.com',
+      linkedin: 'https://www.linkedin.com/in/tula-mrudhul/',
+      email: 'b23ee1076@iitj.ac.in',
       github: '#'
     
     }
@@ -100,11 +100,11 @@ const teamMembers = [
     name: 'Sohom Sarkar',
     role: 'Core Team',
     department: 'Student Team',
-    image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
+    image: 'https://media.licdn.com/dms/image/v2/D4E03AQFSanrNDYTZrw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1718326250220?e=1762992000&v=beta&t=XDfKKm11tzDphKpDP3lHKA9AtYVyH9h44oIE2AuFBB0',
     bio: 'Member of the Core Team',
     social: {
-      linkedin: '#',
-      email: 'sohom@example.com',
+      linkedin: 'https://www.linkedin.com/in/sohom-sarkar-598b9b2aa/',
+      email: 'b23ee1099@iitj.ac.in',
       github: '#'
     }
   },
@@ -113,11 +113,11 @@ const teamMembers = [
     name: 'Agam Harpreet Singh',
     role: 'Core Team',
     department: 'Student Team',
-    image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
+    image: 'https://media.licdn.com/dms/image/v2/D5635AQHWPHjZJeuwjA/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1735400559815?e=1760961600&v=beta&t=J4rWTOcqpXiiXE41uLVE6Rag33JpPaoeiSse39rPhnk',
     bio: 'Member of the Core Team',
     social: {
-      linkedin: '#',
-      email: 'agam@example.com',
+      linkedin: 'https://www.linkedin.com/in/agam-harpreet-singh/',
+      email: 'b23cm1004@iitj.ac.in',
       github: '#'
     }
   },
@@ -126,11 +126,11 @@ const teamMembers = [
     name: 'Goutham A.S',
     role: 'Core Team',
     department: 'Student Team',
-    image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
+    image: 'https://media.licdn.com/dms/image/v2/D4E03AQEkWruKoKW3tQ/profile-displayphoto-scale_400_400/B4EZfqYzDBHsAk-/0/1751984061226?e=1762992000&v=beta&t=I8foTC_AidEO_eNMyOyyG_xaMJR19hteihH3EGMAKAE',
     bio: 'Member of the Core Team',
     social: {
-      linkedin: '#',
-      email: 'goutham@example.com',
+      linkedin: 'https://www.linkedin.com/in/goutham-a-s-93b30b312/overlay/contact-info/',
+      email: 'b23ee1024@iitj.ac.in',
       github: '#'
     }
   },
@@ -139,11 +139,11 @@ const teamMembers = [
     name: 'Tashir Ahmed',
     role: 'Core Team',
     department: 'Student Team',
-    image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
+    image: 'https://media.licdn.com/dms/image/v2/D5603AQGw4jLMISBT_w/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1703167504983?e=1762992000&v=beta&t=cXgZGZZ2CwZmdzJMANKuIVS082IGnG_gDtrfp_k-_wQ',
     bio: 'Member of the Core Team',
     social: {
-      linkedin: '#',
-      email: 'tashir@example.com',
+      linkedin: 'https://www.linkedin.com/in/tashir-ahmed-5315b128b/',
+      email: 'b23me1074@iitj.ac.in',
       github: '#'
     }
   },
@@ -152,53 +152,53 @@ const teamMembers = [
     name: 'Raghuveer Kulkarni',
     role: 'Core Team',
     department: 'Student Team',
-    image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
+    image: 'https://media.licdn.com/dms/image/v2/D4D35AQEKx1Fn6_tjUg/profile-framedphoto-shrink_400_400/B4DZlileT0GsAc-/0/1758295615083?e=1760961600&v=beta&t=pgMJXO5W5RWwY7G3jZircQB1CThjRIPzqWVJZoObWb0',
     bio: 'Member of the Core Team',
     social: {
-      linkedin: '#',
-      email: 'raghuveer@example.com',
+      linkedin: 'https://www.linkedin.com/in/raghuveer-kulkarni-185450281/',
+      email: 'b23es1020@iitj.ac.in',
       github: '#'
     }
-  },
-  {
-    id: 13,
-    name: 'Pragay',
-    role: 'Core Team',
-    department: 'Student Team',
-    image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
-    bio: 'Member of the Core Team',
-    social: {
-      linkedin: '#',
-      email: 'pragay@example.com',
-      github: '#'
-    }
-  },
-  {
-    id: 14,
-    name: 'Nisarg Upadhyaya',
-    role: 'Core Team',
-    department: 'Student Team',
-    image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
-    bio: 'Member of the Core Team',
-    social: {
-      linkedin: '#',
-      email: 'nisarg@example.com',
-      github: '#'
-    }
-  },
-  {
-    id: 15,
-    name: 'Aradhya Mahajan',
-    role: 'Core Team',
-    department: 'Student Team',
-    image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
-    bio: 'Member of the Core Team',
-    social: {
-      linkedin: '#',
-      email: 'aradhya@example.com',
-      github: '#'
-    }
-  },
+  // },
+  // {
+  //   id: 13,
+  //   name: 'Pragay',
+  //   role: 'Core Team',
+  //   department: 'Student Team',
+  //   image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
+  //   bio: 'Member of the Core Team',
+  //   social: {
+  //     linkedin: '#',
+  //     email: 'pragay@example.com',
+  //     github: '#'
+  //   }
+  // },
+  // {
+  //   id: 14,
+  //   name: 'Nisarg Upadhyaya',
+  //   role: 'Core Team',
+  //   department: 'Student Team',
+  //   image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
+  //   bio: 'Member of the Core Team',
+  //   social: {
+  //     linkedin: '#',
+  //     email: 'nisarg@example.com',
+  //     github: '#'
+  //   }
+  // },
+  // {
+  //   id: 15,
+  //   name: 'Aradhya Mahajan',
+  //   role: 'Core Team',
+  //   department: 'Student Team',
+  //   image: 'https://t4.ftcdn.net/jpg/05/89/93/27/360_F_589932782_vQAEAZhHnq1QCGu5ikwrYaQD0Mmurm0N.jpg',
+  //   bio: 'Member of the Core Team',
+  //   social: {
+  //     linkedin: '#',
+  //     email: 'aradhya@example.com',
+  //     github: '#'
+  //   }
+  // },
   
 ];
 
